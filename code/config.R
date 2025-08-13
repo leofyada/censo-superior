8ebc3b176d1f27a287b969e9d0b2151ee995327e
Leonardo Fujisima Yada <leofyada@gmail.com>
2025-08-13 15:37:08 -0300

    Cria arquivo para importar bibliotecas utilizadas no projeto

---
+++ code/config.R
@@ -0,0 +1,18 @@
+#------------------------
+#- Importar bibliotecas - 
+#------------------------
+
+# Função para importar pacotes
+importar_pacotes <- function(pacotes) {
+  for (pacote in pacotes) {
+    if (!require(pacote, character.only = TRUE)) {
+      message(paste("Pacote", pacote, "não encontrado. Instalando..."))
+    } else {
+      message(paste("Pacote", pacote, "carregado com sucesso."))
+    }
+  }
+}
+# Pacotes utilizados
+pacotes <- c("here", "arrow", "dplyr", "archive", "aws.s3", "data.table", "aws.signature")
+# Importa pacotes selecionados
+importar_pacotes(pacotes)
