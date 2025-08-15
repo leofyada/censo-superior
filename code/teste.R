d4d07f36869113fb92faccb05e9f32065b697be5
Leonardo Fujisima Yada <leofyada@gmail.com>
2025-08-14 22:19:19 -0300

    Atualiza scripts de validação e transformação de dados

--- code/teste.R
+++ code/teste.R
@@ -8,4 +8,27 @@ regras <- validator(
   sum(QT_VG_TOTAL) == 19181871, 
   # Total de matrículas
   sum(QT_MAT) == 4913281,       
-  # Total de matrículas à distância em cursos de graduação no estado d
+  # Total de matrículas à distância em cursos de graduação no estado do Paraná
+  sum(QT_MAT[SG_UF_IES=="PR" & TP_NIVEL_ACADEMICO==1 & TP_DIMENSAO == 2]) == 1773058,
+  # Total de ingressantes em cursos de graduação à distância
+  sum(QT_ING[TP_DIMENSAO %in% c(2, 3, 4)]) == 3314402,
+  # Total de matrículas em cursos de pedagogia
+  sum(subset(df_prata, NO_CINE_ROTULO=="Pedagogia" & TP_DIMENSAO %in% c(2, 3, 4))$QT_MAT),
+  # Total de cursos de graduação a distância
+  sum(QT_CURSO) == 10554
+)
+
+# Análise de atendimento às regras
+resultado <- confront(dat = df_prata, x = regras)
+resultado
+
+#--------------
+#- EXPORTAÇÃO -
+#--------------
+
+# Caminho para exportar a base padrão prata
+caminho_df_prata <- here("data", "prata", glue("{as.integer(format(Sys.Date(), '%Y%m%d'))}_base_final.xlsx"))
+# Exporta arquivo
+writexl::write_xlsx(df_prata, path = caminho_df_prata)
+
+
--- code/transformacao.R
+++ code/transformacao.R
@@ -5,4 +5,88 @@
 # Cria uma conexão que representa o banco de dados
 con <- dbConnect(duckdb::duckdb(), dbdir = ":memory:")
 # Instala extensão para conexão com S3
-dbExecute(con, "INSTALL httpfs; LOAD httpfs;
+dbExecute(con, "INSTALL httpfs; LOAD httpfs;")
+# Cria arquivo padrão prata
+df_prata <- dbGetQuery(
+  con,
+  "
+    WITH tab_cursos AS (
+      SELECT 
+        CO_IES, 
+        NO_CURSO, 
+        CO_CURSO, 
+        CO_CINE_AREA_GERAL, 
+        NO_CINE_AREA_GERAL,
+        CO_CINE_ROTULO,
+        NO_CINE_ROTULO,
+        CO_CINE_AREA_ESPECIFICA,
+        NO_CINE_AREA_ESPECIFICA,
+        CO_CINE_AREA_DETALHADA,
+        NO_CINE_AREA_DETALHADA,
+        TP_GRAU_ACADEMICO,
+        TP_NIVEL_ACADEMICO,
+        TP_DIMENSAO,
+        SUM(QT_VG_TOTAL) AS 'QT_VG_TOTAL', 
+        SUM(QT_CONC) AS 'QT_CONC_TOTAL',
+        SUM(QT_MAT) AS 'QT_MAT',
+        SUM(QT_ING) AS 'QT_ING',
+        SUM(QT_CURSO) AS 'QT_CURSO'
+      FROM read_parquet('s3://profissaodocente-inepdata-s3/inep/bronze/microsuperior2023_cur/TP_MODALIDADE_ENSINO=2/part-0.parquet')
+      GROUP BY 
+        CO_IES, 
+        NO_CURSO, 
+        CO_CURSO, 
+        CO_CINE_AREA_GERAL, 
+        NO_CINE_AREA_GERAL, 
+        CO_CINE_ROTULO,
+        NO_CINE_ROTULO,
+        CO_CINE_AREA_ESPECIFICA,
+        NO_CINE_AREA_ESPECIFICA,
+        CO_CINE_AREA_DETALHADA,
+        NO_CINE_AREA_DETALHADA,
+        TP_GRAU_ACADEMICO, 
+        TP_NIVEL_ACADEMICO, 
+        TP_DIMENSAO
+    ), 
+    tab_ies AS (
+      SELECT 
+        CO_UF_IES,
+        SG_UF_IES,
+        CO_MUNICIPIO_IES,
+        NO_MUNICIPIO_IES,
+        CO_IES,
+        NO_IES,
+        SG_IES
+      FROM read_parquet('s3://profissaodocente-inepdata-s3/inep/bronze/microsuperior2023_ies/part-0.parquet')
+    )
+    SELECT 
+      CO_UF_IES,
+      SG_UF_IES,
+      CO_MUNICIPIO_IES,
+      NO_MUNICIPIO_IES,
+      tab_ies.CO_IES,
+      NO_IES,
+      SG_IES,
+      NO_CURSO, 
+      CO_CURSO, 
+      CO_CINE_AREA_GERAL, 
+      NO_CINE_AREA_GERAL, 
+      CO_CINE_ROTULO,
+      NO_CINE_ROTULO,
+      CO_CINE_AREA_ESPECIFICA,
+      NO_CINE_AREA_ESPECIFICA,
+      CO_CINE_AREA_DETALHADA,
+      NO_CINE_AREA_DETALHADA,
+      TP_GRAU_ACADEMICO,
+      TP_NIVEL_ACADEMICO,
+      TP_DIMENSAO, 
+      QT_VG_TOTAL, 
+      QT_CONC_TOTAL,
+      QT_MAT,
+      QT_ING, 
+      QT_CURSO
+    FROM tab_cursos
+    INNER JOIN tab_ies ON tab_ies.CO_IES = tab_cursos.CO_IES;
+  "
+)
+
