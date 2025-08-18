1b47f9c3bdad6d5d99c46b564d4070c3676d1b4e
Leonardo Fujisima Yada <leofyada@gmail.com>
2025-08-18 12:07:34 -0300

    Converte scripts para funções

--- code/extracao.R
+++ code/extracao.R
@@ -2,14 +2,28 @@
 #- Extração da pasta ZIP com dados do Censo da Educação Superior - 
 #-----------------------------------------------------------------
 
-# Caminho para a pasta ZIP
-caminho_censosuperior <- "https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_2023.zip"
-# Caminho de destino da pasta baixada
-destino_censosuperior <- here("data", "censosuperior_23.zip")
-# Download
-download.file(url = caminho_censosuperior, destfile = destino_censosuperior, mode = "wb") 
-# Caminho para os arquivos
-destino_unzip_censosuperior <- here("data", "unzip")
-# Unzip 
-archive::archive_extract(archive = destino_censosuperior, dir = destino_unzip_censosuperior)
+# Função que realiza o download dos dados do Censo e extrai os dados do ZIP
+funcao_extracao <- function(caminho_censosuperior) {
+  # Definição do destino da pasta 
+  destino_censosuperior <- here("data", "bronze", "censosuperior_23.zip")
+  # Download e unzip dos dados
+  tryCatch({
+    # Download dos dados
+    download.file(url = caminho_censosuperior, destfile = destino_censosuperior, mode = "wb")
+    cat("Download realizado com sucesso!\n")
+    # Caminho para os arquivos
+    destino_unzip_censosuperior <- here("data", "bronze", "unzip")
+    # Unzip 
+    archive::archive_extract(archive = destino_censosuperior, dir = destino_unzip_censosuperior)
+    cat("Extração dos dados realizada com sucesso!\n")
+  }, error = function(e) {
+    # Erro
+    cat("Ocorrou o seguinte erro:", conditionMessage(e), "\n")
+  }, warning = function(w) {
+    # Aviso
+    cat("Um aviso apareceu:", conditionMessage(w), "\n")
+  })
+}
+
+
 
---
+++ code/main.R
@@ -0,0 +1,30 @@
+#-------------------------------------------
+#- Função de execução do pipeline de dados - 
+#-------------------------------------------
+
+# Importação das funções
+source("code/config.R")
+source(here("code", "extracao.R"))
+source(here("code", "carregamento.R"))
+source(here("code", "transformacao.R"))
+source(here("code", "validacao.R"))
+
+# Caminho dos dados do Censo da Educação Superior
+caminho_censosuperior <- "https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_2023.zip"
+
+# Criação de função do pipeline de dados
+funcao_pipeline <- function(caminho_censosuperior) {
+  # Extração dos dados do Censo da Educação Superior
+  funcao_extracao(caminho_censosuperior)
+  # Carregamento para o bucket S3
+  funcao_carregamento()
+  # Modelagem de dados
+  df_prata <- funcao_transformacao()
+  # Validação e exportação
+  funcao_validacao(df_prata)
+}
+
+# Execução de pipeline de dados
+funcao_pipeline(caminho_censosuperior)
+
+
